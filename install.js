7d7a03503c661eec5abb79dd9351134bff611631
Rebecca Smith <rebecca.smith@hotmail.se>
2025-06-18 18:34:34 +0200

    Created installationsfile with one table.  Connected to sqlite3

---
+++ install.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+// Connect to database
+
+const db = new sqlite3.Database(process.env.DATABASE);
+
+// Create tables users
+
+db.serialize(() => {
+//Drop table 
+
+db.run("DROP TABLE IF EXISTS users");
+
+//Create table, bra med id så man inte behöver använda användarnamnet när man pratar om sina användare
+db.run(`CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP
+)`
+);
+console.log("Table created");
+
+
+});

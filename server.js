c1d183114bd25cb5f506bab46ec619361be4a802
Rebecca Smith <rebecca.smith@hotmail.se>
2025-08-03 15:37:24 +0200

    Lagt till cors

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const express = require("express");
 const bodyParser = require ("body-parser");
 const authRoutes =require("./routes/authRoutes")
 const jwt = require("jsonwebtoken")
+const cors = require ("cors")
 require("dotenv").config();
 
 const port=process.env.PORT||3000;
 
+
+
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors());
 
 //Routes
 app.use("/api", authRoutes);
